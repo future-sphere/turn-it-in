f0854e760530cb6ec6c356733e0697b72f2a6c91
kaina1129 <zhazhe9494@gmail.com>
2020-02-19 22:15:07 -0500

    revised hw

--- week5/kaina/qz/index.js
+++ week5/kaina/qz/index.js
@@ -1,9 +1,9 @@
-document.getElementById("quick-login").style.display = "none";
+document.getElementById('quick-login').style.display = 'none';
 const handleClick = () => {
-  document.getElementById("wrapper").style.display = "none";
-  document.getElementById("quick-login").style.display = "block";
+	document.getElementById('wrapper').style.display = 'none';
+	document.getElementById('quick-login').style.display = 'block';
 };
 const handleSwitchBack = () => {
-  document.getElementById("wrapper").style.display = "block";
-  document.getElementById("quick-login").style.display = "none";
+	document.getElementById('wrapper').style.display = 'block';
+	document.getElementById('quick-login').style.display = 'none';
 };
