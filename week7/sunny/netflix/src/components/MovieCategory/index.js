c9cbe695f028fa1ce8c33b197a2b6b21847ed7b1
sunniiiw <yuqingsunnywang@gmail.com>
2020-02-22 15:28:10 -0500

    modified

--- week7/sunny/netflix/src/components/MovieCategory/index.js
+++ week7/sunny/netflix/src/components/MovieCategory/index.js
@@ -35,34 +35,6 @@ export default function MovieCategory() {
 						<p>Émissions TV</p>
 						<i class='fas fa-angle-down'></i>
 					</div>
-					{/* <div className='category red'>
-						<p>Comédie</p>
-						<i class='fas fa-angle-down'></i>
-					</div>
-					<div className='category orange'>
-						<p>Action</p>
-						<i class='fas fa-angle-down'></i>
-					</div>
-					<div className='category blue'>
-						<p>Drame</p>
-						<i class='fas fa-angle-down'></i>
-					</div>
-					<div className='category purple'>
-						<p>Sci-Fi</p>
-						<i class='fas fa-angle-down'></i>
-					</div>
-					<div className='category pink'>
-						<p>Romance</p>
-						<i class='fas fa-angle-down'></i>
-					</div>
-					<div className='category green'>
-						<p>Thriller</p>
-						<i class='fas fa-angle-down'></i>
-					</div>
-					<div className='category mint'>
-						<p>Émissions TV</p>
-						<i class='fas fa-angle-down'></i>
-					</div> */}
 				</div>
 			</div>
 		</div>
