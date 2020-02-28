1d01e3dd049542fc41858186465f6435c05146fd
wsh941102 <willsihangw@gmail.com>
2020-02-27 23:42:31 -0500

    add week8 homework

---
+++ week8/will/will-todolist/src/components/List/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './index.scss';
+import classnames from 'classnames';
+
+const list = ({ todo, handleListColor, handleListDelete }) => {
+	return (
+		<div className='list-container'>
+			<h2>To &thinsp;Do &thinsp;List</h2>
+			<div className='list-block'>
+				{todo.map((v, i) => (
+					<div className={classnames('list', { active: v.isActive })}>
+						<div>
+							<button onClick={() => handleListColor(i)}>
+								<i class='fas fa-check-circle'></i>
+							</button>
+							<span>{v}</span>
+							<button onClick={() => handleListDelete(i)}>
+								<i class='fas fa-times-circle'></i>
+							</button>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default list;
